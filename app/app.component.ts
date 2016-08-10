72e9e4633097c96f3115e5f3e90e58c63468cabd
Silin Na <n1207n@gmail.com>
2016-08-10 15:45:10 -0700

    feat: Added multi-line template for displaying hero info

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,16 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1> <h2>{{hero.name}} details!</h2>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
+        <label>name: </label>{{hero.name}}
+      </div>
+      `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
