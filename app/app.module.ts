4b9d40780c15dfc5502d07193396e6ac146f517e
Silin Na <n1207n@gmail.com>
2016-08-10 16:51:25 -0700

    refactor: Renamed AppComponent as HeroesComponent
    + Added a new AppComponent as the container
    
    + Moved HeroService provider level to root ngModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  providers: [ HeroService ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
