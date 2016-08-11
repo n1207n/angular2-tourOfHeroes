a2f98bd0be02482b9c0f18138bec6ed6963df6d8
Silin Na <n1207n@gmail.com>
2016-08-10 17:11:48 -0700

    feat: Used templateUrl for DashboardComponent
    + Added HeroService singleton to DashboardComponent to get top 5 heroes

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My dashboard</h3>'
+  templateUrl: 'app/dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService) {
+
+  }
+
+  ngOnInit() {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  gotoDetails() {
+    
+  }
 }
