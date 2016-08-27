dae5b0abba9be75ce110e8b397c98f9295ab85da
Silin Na <n1207n@gmail.com>
2016-08-27 09:27:09 -0700

    fix: Fixed the typo of routerlink

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
     <h1>{{title}}</h1>
 
     <nav>
-      <a routerlink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerlink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
     </nav>
 
     <router-outlet></router-outlet>
